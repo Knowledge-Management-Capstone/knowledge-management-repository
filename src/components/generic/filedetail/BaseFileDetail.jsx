1d329e02b905788b500724db223683d01f64ad7b
DzakiyH <dzakiy.h@mail.ugm.ac.id>
2022-12-04 16:13:58 +0700

    fixed document search result

--- src/components/generic/filedetail/BaseFileDetail.jsx
+++ src/components/generic/filedetail/BaseFileDetail.jsx
@@ -6,6 +6,7 @@ import {
 } from '@heroicons/react/outline';
 
 function BaseFileDetail({ data }) {
+  const date = data.updatedAt.split('T')[0];
   return (
     <div className='flex flex-row justify-between py-3 items-center h-44'>
       <DocumentTextIcon className='h-[90%] basis-1/7' />
@@ -18,16 +19,16 @@ function BaseFileDetail({ data }) {
       </div>
       <div className='flex justify-around items-end flex-col basis-1/4 h-full ml-6'>
         <div className='flex flex-row'>
-          <CheckCircleIcon className='w-6 mr-2' />
-          <div>Completed</div>
+          <div>{data.status}</div>
+          <CheckCircleIcon className='w-6 ml-2' />
         </div>
         <div className='flex flex-row'>
-          <DocumentTextIcon className='w-6 mr-2' />
-          <div>Completed</div>
+          <div>{data.extension}</div>
+          <DocumentTextIcon className='w-6 ml-2' />
         </div>
         <div className='flex flex-row'>
-          <ClockIcon className='w-6 mr-2' />
-          <div>Completed</div>
+          <div>{date}</div>
+          <ClockIcon className='w-6 ml-2' />
         </div>
       </div>
     </div>
